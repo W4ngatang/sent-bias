c8230af7204279c44454f129df849d08190dfb87
Chandler May <chandler.j.may@gmail.com>
2018-11-30 13:13:12 -0500

    Generate sentence-level, word-level version of Heilman tests.

--- scripts/generate_heilman_double_bind.sh
+++ scripts/generate_heilman_double_bind.sh
@@ -8,8 +8,14 @@ likable="agreeable fair honest trustworthy selfless accommodating likable liked"
 unlikable="abrasive conniving manipulative dishonest selfish pushy unlikable unliked"
 male_names="John Paul Mike Kevin Steve Greg Jeff Bill"
 female_names="Amy Joan Lisa Sarah Diana Kate Ann Donna"
-ambig_success_text='NAME is the assistant vice president of sales at an aircraft company, and is in charge of training and supervising junior executives, breaking into new markets, keeping abreast of industry trends, and generating new clients.  The products PRON is responsible for include engine assemblies, fuel tanks, and other aircraft equipment and parts.  CAP_PRON is about to undergo POSS_PRON annual performance review; POSS_PRON evaluation will be based on sales volume, number of new client accounts, and actual dollars earned.'
-clear_success_text='NAME is the assistant vice president of sales at an aircraft company, and is in charge of training and supervising junior executives, breaking into new markets, keeping abreast of industry trends, and generating new clients.  The products PRON is responsible for include engine assemblies, fuel tanks, and other aircraft equipment and parts.  CAP_PRON has recently undergone the company-wide annual performance review and PRON received consistently high evaluations.  CAP_PRON has been designated as a "stellar performer" based on sales volume, number of new client accounts, and actual dollars earned.  CAP_POSS_PRON performance is in the top 5% of all employees at POSS_PRON level.'
+long_ambig_success_text='NAME is the assistant vice president of sales at an aircraft company, and is in charge of training and supervising junior executives, breaking into new markets, keeping abreast of industry trends, and generating new clients.  The products PRON is responsible for include engine assemblies, fuel tanks, and other aircraft equipment and parts.  CAP_PRON is about to undergo POSS_PRON annual performance review; POSS_PRON evaluation will be based on sales volume, number of new client accounts, and actual dollars earned.'
+long_clear_success_text='NAME is the assistant vice president of sales at an aircraft company, and is in charge of training and supervising junior executives, breaking into new markets, keeping abreast of industry trends, and generating new clients.  The products PRON is responsible for include engine assemblies, fuel tanks, and other aircraft equipment and parts.  CAP_PRON has recently undergone the company-wide annual performance review and PRON received consistently high evaluations.  CAP_PRON has been designated as a "stellar performer" based on sales volume, number of new client accounts, and actual dollars earned.  CAP_POSS_PRON performance is in the top 5% of all employees at POSS_PRON level.'
+long_role="assistant vice president"
+medium_ambig_success_text='NAME is an engineer.'
+medium_clear_success_text='NAME is an engineer with superior technical skills.'
+medium_role=engineer
+short_text=NAME
+short_role=engineer
 
 print_targets() {
     local success_text="$1"
@@ -36,7 +42,7 @@ print_targets() {
             sed "s/POSS_PRON/$poss_pron/g" | \
             sed "s/CAP_PRON/$cap_pron/g;s/PRON/$pron/g" | \
             cut -d . -f "$sentences_to_keep"`
-        if [ "${field: -1}" != . ]
+        if [ "${success_text: -1}" == . -a "${field: -1}" != . ]
         then
             field="$field."
         fi
@@ -48,11 +54,20 @@ print_targets() {
 print_attributes() {
     local adjectives="$1"
     shift
+    local role="$1"
+    shift
+    local attribute_context="$1"
+    shift
 
     local adjective=
     for adjective in $adjectives
     do
-        echo -en "\tThe assistant vice president is $adjective."
+        if [ "$attribute_context" == word ]
+        then
+            echo -en "\t$adjective"
+        else
+            echo -en "\tThe $role is $adjective."
+        fi
     done
     echo
 }
@@ -75,6 +90,10 @@ print_tests() {
     shift
     local sentences_to_keep="$1"
     shift
+    local role="$1"
+    shift
+    local attribute_context="$1"
+    shift
 
     print_header
     echo -en "targ1\tMale"
@@ -82,23 +101,61 @@ print_tests() {
     echo -en "targ2\tFemale"
     print_targets "$success_text" "$female_names" Her her She she "$sentences_to_keep"
     echo -en "attr1\t$attribute_1_name"
-    print_attributes "$attribute_1"
+    print_attributes "$attribute_1" "$role" "$attribute_context"
     echo -en "attr2\t$attribute_2_name"
-    print_attributes "$attribute_2"
+    print_attributes "$attribute_2" "$role" "$attribute_context"
 }
 
 for sentences_to_keep in 1 1- 1,3-
 do
     print_tests \
-        "$ambig_success_text" \
+        "$long_ambig_success_text" \
         CompetentAchievementOriented "$competent" \
         IncompetentNotAchievementOriented "$incompetent" \
         $sentences_to_keep \
-        > ../tests/heilman_double_bind_ambiguous_$sentences_to_keep.txt
+        "$long_role" \
+        sent \
+        > ../tests/heilman_double_bind_competent_$sentences_to_keep.txt
     print_tests \
-        "$clear_success_text" \
+        "$long_clear_success_text" \
         LikableNotHostile "$likable" \
         UnlikableHostile "$unlikable" \
         $sentences_to_keep \
-        > ../tests/heilman_double_bind_clear_$sentences_to_keep.txt
+        "$long_role" \
+        sent \
+        > ../tests/heilman_double_bind_likable_$sentences_to_keep.txt
 done
+
+print_tests \
+    "$medium_ambig_success_text" \
+    CompetentAchievementOriented "$competent" \
+    IncompetentNotAchievementOriented "$incompetent" \
+    1- \
+    "$medium_role" \
+    sent \
+    > ../tests/heilman_double_bind_competent_one_sentence.txt
+print_tests \
+    "$medium_clear_success_text" \
+    LikableNotHostile "$likable" \
+    UnlikableHostile "$unlikable" \
+    1- \
+    "$medium_role" \
+    sent \
+    > ../tests/heilman_double_bind_likable_one_sentence.txt
+
+print_tests \
+    "$short_text" \
+    CompetentAchievementOriented "$competent" \
+    IncompetentNotAchievementOriented "$incompetent" \
+    1- \
+    "$short_role" \
+    word \
+    > ../tests/heilman_double_bind_competent_one_word.txt
+print_tests \
+    "$short_text" \
+    LikableNotHostile "$likable" \
+    UnlikableHostile "$unlikable" \
+    1- \
+    "$short_role" \
+    word \
+    > ../tests/heilman_double_bind_likable_one_word.txt
