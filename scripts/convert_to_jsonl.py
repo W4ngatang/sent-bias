d26a5f92e17dfe06b616db885151a81b71aa3126
Chandler May <chandler.j.may@gmail.com>
2018-11-30 20:07:17 +0000

    Add/reorganize double bind tests; format and move convert_to_json to scripts.

---
+++ scripts/convert_to_jsonl.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+
+def convert_file(filename):
+    print("Converting %s..." % filename)
+    data = {}
+    with open("%s.txt" % filename) as filehandle:
+        for raw in filehandle:
+            if raw[0] == "#":
+                continue
+            row = raw.strip().split('\t')
+            data[row[0]] = {"category": row[1], "examples": row[2:]}
+
+    with open("%s.jsonl" % filename, 'w') as out_fh:
+        json.dump(data, out_fh, indent=2)
+
+
+if __name__ == '__main__':
+    files = []
+    for entry in os.listdir('.'):
+        if not entry.startswith('.') and entry.endswith('.txt'):
+            files.append(entry[:-len('.txt')])
+    for f in files:
+        convert_file(f)
