ef772f7ca4a74c19b77063dac789d0b965864edf
w4ngatang <wang.alex.c@gmail.com>
2018-12-03 10:38:38 -0500

    Add seeds and remove module calls

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,37 +4,37 @@ source user_config.sh
 
 echo 'Note: this script should be called from the root of the repository' >&2
 
-#TESTS=weat1,weat2,weat3,weat3b,weat4,weat5,weat5b,weat6,weat6b,weat7,weat7b,weat8,weat8b,weat9,weat10,sent-weat1,sent-weat2,sent-weat3,sent-weat4,angry_black_woman_stereotype,angry_black_woman_stereotype_b,heilman_double_bind_ambiguous_1,heilman_double_bind_clear_1,heilman_double_bind_ambiguous_1+3-,heilman_double_bind_clear_1+3-
-TESTS=weat1,weat2,weat3,weat4
+TESTS=weat1,weat2,weat3,weat3b,weat4,weat5,weat5b,weat6,weat6b,weat7,weat7b,weat8,weat8b,weat9,weat10,sent-weat1,sent-weat2,sent-weat3,sent-weat4,angry_black_woman_stereotype,angry_black_woman_stereotype_b,heilman_double_bind_ambiguous_1,heilman_double_bind_clear_1,heilman_double_bind_ambiguous_1+3-,heilman_double_bind_clear_1+3-
+#TESTS=weat1,weat2,weat3,weat4
 set -e
 
 SEED=2222
 
 # debug
-#python -m ipdb sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} -m bert --bert_version large --exp_dir ${SAVE_DIR} --data_dir tests/ --glove_path ${GLOVE_PATH} --combine_method max
+#python ipdb sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} bert --bert_version large --exp_dir ${SAVE_DIR} --data_dir tests/ --glove_path ${GLOVE_PATH} --combine_method max 
 
 # BoW (consumes GloVe method)
-#python -m sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} --exp_dir ${SAVE_DIR} --data_dir tests/ -m bow --glove_path ${GLOVE_PATH}
+#python sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} --exp_dir ${SAVE_DIR} --data_dir tests/ bow --glove_path ${GLOVE_PATH} -s ${SEED}
 
 # SkipThoughts
 
 # InferSent
-#python -m sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} --exp_dir ${SAVE_DIR} --data_dir tests/ -m infersent --infersent_dir sentbias/encoders --glove_path ${GLOVE_PATH}
+#python sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} --exp_dir ${SAVE_DIR} --data_dir tests/ infersent --infersent_dir sentbias/encoders --glove_path ${GLOVE_PATH} -s ${SEED}
 
 # GenSen
-#python -m sentbias/main.py --log_file ${SAVE_DIR}/log.log -t weat1,weat2,weat3,weat4 --exp_dir ${SAVE_DIR} --data_dir tests/ -m gensen --gensen_dir ${GENSEN_DIR} --glove_path ${GLOVE_PATH}
+#python sentbias/main.py --log_file ${SAVE_DIR}/log.log -t weat1,weat2,weat3,weat4 --exp_dir ${SAVE_DIR} --data_dir tests/ gensen --gensen_dir ${GENSEN_DIR} --glove_path ${GLOVE_PATH} -s ${SEED}
 
 # GUSE
-#python -m sentbias/main.py --log_file ${SAVE_DIR}/log.log -t weat1,weat2,weat3,weat4 --exp_dir ${SAVE_DIR} --data_dir tests/ -m guse 
+#python sentbias/main.py --log_file ${SAVE_DIR}/log.log -t weat1,weat2,weat3,weat4 --exp_dir ${SAVE_DIR} --data_dir tests/ guse -s ${SEED}
 
 # CoVe
-#python -m sentbias/main.py --log_file ${SAVE_DIR}/log.log -t weat1,weat2,weat3,weat4 -m cove --exp_dir ${SAVE_DIR} --data_dir tests/ --cove_encs encodings/cove/
+#python sentbias/main.py --log_file ${SAVE_DIR}/log.log -t weat1,weat2,weat3,weat4 cove --exp_dir ${SAVE_DIR} --data_dir tests/ --cove_encs encodings/cove/ -s ${SEED}
 
 # ELMo
-python -m sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} --exp_dir ${SAVE_DIR} --data_dir tests/ -m elmo --combine_method max --elmo_combine add -s ${SEED}
+python sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} --exp_dir ${SAVE_DIR} --data_dir tests/ elmo --combine_method max --elmo_combine add -s ${SEED}
 
 # OpenAI GPT
-#python -m sentbias/main.py --log_file ${SAVE_DIR}/log.log -t weat1,weat2,weat3,weat4 -m openai --exp_dir ${SAVE_DIR} --data_dir tests/ --openai_encs openai_encs/
+#python sentbias/main.py --log_file ${SAVE_DIR}/log.log -t weat1,weat2,weat3,weat4 openai --exp_dir ${SAVE_DIR} --data_dir tests/ --openai_encs openai_encs/ -s ${SEED}
 
 # BERT
-#python -m sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} -m bert --bert_version large --exp_dir ${SAVE_DIR} --data_dir tests/ --glove_path ${GLOVE_PATH} --combine_method max
+#python sentbias/main.py --log_file ${SAVE_DIR}/log.log -t ${TESTS} bert --bert_version large --exp_dir ${SAVE_DIR} --data_dir tests/ --glove_path ${GLOVE_PATH} --combine_method max -s ${SEED}
