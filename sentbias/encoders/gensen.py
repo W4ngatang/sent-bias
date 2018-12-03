8abb20c269ef1525d86029214b2e46c1fff0b31f
Chandler May <chandler.j.may@gmail.com>
2018-12-02 20:48:55 -0500

    Run autopep8.

--- sentbias/encoders/gensen.py
+++ sentbias/encoders/gensen.py
@@ -12,21 +12,24 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
+
 def encode(model, sents):
     ''' Use model to encode gensen sents '''
     sent2vec = {}
     reps_h, reps_h_t = model.get_representation(sents, pool='last', return_numpy=True, tokenize=True)
-    for j in range(0,len(sents)):
+    for j in range(0, len(sents)):
         sent2vec[sents[j]] = reps_h_t[j]
     return sent2vec
 
+
 def build_vocab(sentences):
-    vocab =[]
+    vocab = []
     for s in sentences:
-        words = s.replace('.',' ').split()
+        words = s.replace('.', ' ').split()
         for w in words:
             vocab.append(w)
     return set(vocab)
+
 
 class Encoder(nn.Module):
     """GenSen Encoder."""
@@ -74,7 +77,7 @@ class Encoder(nn.Module):
 
         try:
             self.src_embedding.weight.data.set_(torch.from_numpy(embedding_matrix))
-        except:
+        except BaseException:
             self.src_embedding.weight.data.set_(torch.from_numpy(embedding_matrix).cuda())
 
         self.src_embedding.cuda()
@@ -159,10 +162,10 @@ class GenSenSingle(nn.Module):
         """Load pretrained params."""
         # Read vocab pickle files
 
-        path = os.path.join(self.model_folder,'%s_vocab.pkl' % (self.filename_prefix))
+        path = os.path.join(self.model_folder, '%s_vocab.pkl' % (self.filename_prefix))
 
         print(path)
-        model_vocab = pickle.load(open(path,'rb'),encoding='latin')
+        model_vocab = pickle.load(open(path, 'rb'), encoding='latin')
         self.word2id = model_vocab['word2id']
         self.id2word = model_vocab['id2word']
         self.task_word2id = self.word2id
@@ -199,11 +202,11 @@ class GenSenSingle(nn.Module):
     def first_expansion(self):
         """Traing linear regression model for the first time."""
         # Read pre-trained word embedding h5 file
-        print ('Loading pretrained word embeddings')
+        print('Loading pretrained word embeddings')
         pretrained_embeddings = h5py.File(self.pretrained_emb)
         pretrained_embedding_matrix = pretrained_embeddings['embedding'].value
         pretrain_vocab = \
-            pretrained_embeddings['words_flatten'].value#.split('\n')
+            pretrained_embeddings['words_flatten'].value  # .split('\n')
         pretrain_word2id = {
             word: ind for ind, word in enumerate(pretrain_vocab)
         }
@@ -221,7 +224,7 @@ class GenSenSingle(nn.Module):
                     pretrained_embedding_matrix[pretrain_word2id[word]]
                 )
 
-        print ('Training vocab expansion on model')
+        print('Training vocab expansion on model')
         lreg = LinearRegression()
         lreg.fit(pretrain_train, model_train)
         self.lreg = lreg
@@ -276,8 +279,8 @@ class GenSenSingle(nn.Module):
                     self.model_embedding_matrix[self.word2id['<unk>']]
                 )
 
-        print ('Found %d task OOVs ' % (oov_task))
-        print ('Found %d pretrain OOVs ' % (oov_pretrain))
+        print('Found %d task OOVs ' % (oov_task))
+        print('Found %d pretrain OOVs ' % (oov_pretrain))
         task_embeddings = np.stack(task_embeddings)
         self.encoder.set_pretrained_embeddings(task_embeddings)
         self.vocab_expanded = True
--- sentbias/main.py
+++ sentbias/main.py
@@ -227,7 +227,7 @@ def main(arguments):
                     encs_attr1 = infersent.encode(model, encs["attr1"]["examples"])
                     encs_attr2 = infersent.encode(model, encs["attr2"]["examples"])
 
-                elif model_name =='gensen':
+                elif model_name == 'gensen':
                     if args.glove_path is None:
                         raise Exception('glove_path must be specified for {} model'.format(model_name))
                     if args.gensen_dir is None:
@@ -236,38 +236,34 @@ def main(arguments):
                         raise ValueError('gensen_version can only have one or two elements')
                     prefixes = split_comma_and_check(args.gensen_version, GENSEN_VERSIONS, "gensen_prefix")
                     model_options = 'version=' + args.gensen_version
-                    
+
                     if model is None:
-                        gensen_1=gensen.GenSenSingle(model_folder=os.path.join(args.gensen_dir, 'models'),
-                                                   filename_prefix=prefixes[0],
-                                                   pretrained_emb=os.path.join(args.glove_path, 'glove.840B.300d.h5'),
-                                                     cuda =True)
-                        
-                        model=gensen_1
-                        if(len(prefixes)==2):
-                                gensen_2=GenSenSingle(model_folder=os.path.join(args.gensen_dir, 'models'),
-                                                        filename_prefix=prefixes[1],
-                                                        pretrained_emb=os.path.join(args.glove_path, 'glove.840B.300d.h5'),
-                                                          cuda =True)
-                                model=GenSen(gensen_1,gensen_2)
-                         
-                        
-                        
-                    
-                    vocab = gensen.build_vocab([s for s in sents["targ1"] + sents["targ2"] + sents["attr1"] + sents["attr2"]])
-                    
+                        gensen_1 = gensen.GenSenSingle(
+                            model_folder=os.path.join(
+                                args.gensen_dir, 'models'), filename_prefix=prefixes[0], pretrained_emb=os.path.join(
+                                args.glove_path, 'glove.840B.300d.h5'), cuda=True)
+
+                        model = gensen_1
+                        if(len(prefixes) == 2):
+                            gensen_2 = GenSenSingle(model_folder=os.path.join(args.gensen_dir, 'models'),
+                                                    filename_prefix=prefixes[1],
+                                                    pretrained_emb=os.path.join(args.glove_path, 'glove.840B.300d.h5'),
+                                                    cuda=True)
+                            model = GenSen(gensen_1, gensen_2)
+
+                    vocab = gensen.build_vocab(
+                        [s for s in sents["targ1"] + sents["targ2"] + sents["attr1"] + sents["attr2"]])
+
                     params_senteval = {'task_path': args.gensen_dir, 'usepytorch': True, 'kfold': 10}
                     params_senteval['classifier'] = {'nhid': 0, 'optim': 'adam', 'batch_size': 64,
                                                      'tenacity': 5, 'epoch_size': 4}
 
-                    
                     model.vocab_expansion(vocab)
-                            
+
                     encs_targ1 = gensen.encode(model, sents["targ1"])
                     encs_targ2 = gensen.encode(model, sents["targ2"])
                     encs_attr1 = gensen.encode(model, sents["attr1"])
                     encs_attr2 = gensen.encode(model, sents["attr2"])
-                    
 
                 elif model_name == 'guse':
                     enc = [[] * 512 for _ in range(4)]
