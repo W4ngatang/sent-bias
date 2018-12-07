9d106d7bac9d858474c7ad9abccb6fd244c2728f
Chandler May <chandler.j.may@gmail.com>
2018-12-07 08:01:32 -0500

    Add new gensen qsub script.

---
+++ scripts/qsub/qsub_gensen_3.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -N sentbias_gensen_3
+#$ -l h_rt=24:00:00,num_proc=1,mem_free=16G,ram_free=16G
+
+source ~/.bashrc
+source activate sentbias
+export TFHUB_CACHE_DIR=/export/b02/cmay14/sentbias/tfhub_cache
+suffix=".$(date '+%Y%m%d%H%M%S').${JOB_ID}"
+python sentbias/main.py -m gensen --use_cpu --log_file log$suffix --results_path results.tsv$suffix --glove_h5_path /export/b02/cmay14/sentbias/glove.840B.300d.h5 --gensen_dir /export/b02/cmay14/sentbias/gensen --gensen_version nli_large_bothskip_parse,nli_large_bothskip
