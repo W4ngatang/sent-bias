e1bc6f624d71907ea96d8df6d91179174fdf2d88
Chandler May <chandler.j.may@gmail.com>
2018-12-07 08:18:51 -0500

    Add qsub script for running bert base.

---
+++ scripts/qsub/qsub_bert_4.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -N sentbias_bert_4
+#$ -l h_rt=24:00:00,num_proc=1,mem_free=16G,ram_free=16G
+
+source ~/.bashrc
+source activate sentbias
+export TFHUB_CACHE_DIR=/export/b02/cmay14/sentbias/tfhub_cache
+suffix=".$(date '+%Y%m%d%H%M%S').${JOB_ID}"
+python sentbias/main.py -m bert --use_cpu --log_file log$suffix --results_path results.tsv$suffix --bert_version bert-base-cased
