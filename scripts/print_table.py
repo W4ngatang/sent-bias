ef608625391d8f2254694a71a3640be137f7570c
Chandler May <chandler.j.may@gmail.com>
2018-12-09 15:01:55 -0500

    Add optional grouping of multiple testing correction.

--- scripts/print_table.py
+++ scripts/print_table.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from csv import DictReader
+import logging
 
 
 STAR_THRESHOLD = 0.01
@@ -44,6 +45,17 @@ TESTS = (
 )
 
 
+def holm_bonferroni(results):
+    results_list = sorted(results.items(), key=lambda p: p[1]['p_value'])
+    reject = True
+    for (i, (k, _)) in enumerate(results_list):
+        row = results[k]
+        if row['p_value'] > STAR_THRESHOLD / (len(results_list) - i):
+            reject = False
+        row['reject'] = reject
+    return results
+
+
 def main():
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
     parser = ArgumentParser(
@@ -52,7 +64,11 @@ def main():
     )
     parser.add_argument('results_path',
                         help='Paths to results tsv file.')
+    parser.add_argument('--correct_within_groups', action='store_true',
+                        help='Correct for multiple tests within each test group (Caliskan, ABW, DB)')
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
 
     results = dict()
     with open(args.results_path) as f:
@@ -68,12 +84,26 @@ def main():
                 raise Exception('duplicate key: {}'.format(k))
             results[k] = row
 
-    results_list = sorted(results.items(), key=lambda p: p[1]['p_value'])
-    reject = True
-    for (i, (k, row)) in enumerate(results_list):
-        if row['p_value'] > STAR_THRESHOLD / (len(results_list) - i):
-            reject = False
-        row['reject'] = reject
+    if args.correct_within_groups:
+        logging.info('Computing multiple testing correction within test groups')
+        caliskan_results = holm_bonferroni(dict(
+            ((model, options, test), v) for ((model, options, test), v) in results.items()
+            if 'weat' in test))
+        abw_results = holm_bonferroni(dict(
+            ((model, options, test), v) for ((model, options, test), v) in results.items()
+            if 'angry_black_woman' in test))
+        db_results = holm_bonferroni(dict(
+            ((model, options, test), v) for ((model, options, test), v) in results.items()
+            if 'double_bind' in test))
+
+        if len(results) != len(caliskan_results) + len(abw_results) + len(db_results):
+            raise Exception('number of results does not match sum across groups (after correction)')
+
+        results = caliskan_results
+        results.update(abw_results)
+        results.update(db_results)
+    else:
+        results = holm_bonferroni(results)
 
     for (test_name, test_description, context_level) in TESTS:
         if test_name is None:
